a308373f548dadaecf317ab7ec50da744a68c2f4
PedroMiguelFontes <58701236+PedroMiguelFontes@users.noreply.github.com>
2026-06-11 19:40:53 +0100

    Config

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 try {
  const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
  const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
- await mongoose.connect(uri);
+ await mongoose.connect(uri, {
+    dbName: DB_NAME
+});
  console.log("Connected to MongoDB");
 } catch (error) {
  console.error("Error connecting to MongoDB:", error);
