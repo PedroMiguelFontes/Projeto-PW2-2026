8fece40e3d9b53ba80e6cce337f5bd634a9a016d
PedroMiguelFontes <58701236+PedroMiguelFontes@users.noreply.github.com>
2026-06-15 15:41:02 +0100

    Completed validation

--- Controllers/users.controller.js
+++ Controllers/users.controller.js
@@ -118,8 +118,6 @@ const createUser = async (req, res) => {
             message: error.message
         });
     }
-
-
         res.status(500).json({ message: error.message });
     }
 };
@@ -231,9 +229,21 @@ const validateFuncionario = async (req,res) => {
             return res.status(403).json({message: "Só os admins podem validar funcionários"})
         }        
 
-        if (req.params.role!=='Funcionario') {
-            return res.status(401).json({message: "Este utilizador não é um funcionário"})
+        const query = resolveUserQuery(req.params.id);
+        const updatedUser = await User.findOne(query).select('-password');
+
+        if (updatedUser.tipo!=='Funcionario') {
+            return res.status(400).json({message: "Este utilizador não é um funcionário"})
         }
+        if (!updatedUser) {
+           return res.status(404).json({ message: "Funcionário não existe" });
+        } else if (updatedUser.estado === 'Ativo') {
+           return res.status(401).json({ message: "Funcionário já está validado" });
+        } else {
+            await User.findOneAndUpdate(query, { estado: 'Ativo' }, { new: true });
+            return res.status(200).json({message: "Utilizador suspenso com sucesso"});
+        } 
+
 
     }
     catch (error) {
--- Routes/users.route.js
+++ Routes/users.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllUsers, getUserById, createUser, updateUser, updatePartialUser, suspendUser, unsuspendUser, deleteUser, getUserByField } = require('../Controllers/users.controller.js');
+const { getAllUsers, getUserById, createUser, updateUser, updatePartialUser, suspendUser, unsuspendUser, deleteUser, getUserByField, validateFuncionario } = require('../Controllers/users.controller.js');
 const { verifyToken } = require('../Controllers/auth.controller.js');
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.get('/search', getUserByField);
 router.post('/', createUser);
 router.patch('/:id/suspend', verifyToken, suspendUser);
 router.patch('/:id/unsuspend', verifyToken, unsuspendUser);
+router.patch('/:id/validate',verifyToken,validateFuncionario)
 router.get('/:id', getUserById);
 router.put('/:id', verifyToken, updateUser);
 router.patch('/:id', verifyToken, updatePartialUser);
