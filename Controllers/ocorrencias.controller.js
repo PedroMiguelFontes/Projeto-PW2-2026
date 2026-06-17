484a36dd8bb35654ffd0ee5a9141d691ae5f9a3f
PedroMiguelFontes <58701236+PedroMiguelFontes@users.noreply.github.com>
2026-06-17 03:36:30 +0100

    Fixing some more errors

--- Controllers/ocorrencias.controller.js
+++ Controllers/ocorrencias.controller.js
@@ -149,7 +149,7 @@ const deleteOcorrencia = async (req, res) => {
             return res.status(404).json({ message: "Ocorrência não encontrada" });
         }
 
-        if (req.loggedUserRole == 'Utilizador' && !ocorrencia.user_id.equals(req.loggedUserId)) {
+        if (req.loggedUserRole == 'Utilizador' && req.loggedUserId !== ocorrencia.user_id) {
             return res.status(403).json({ message: "Apenas podes apagar ocorrencias que você criou" });
         }
         await ocorrencia.delete()
